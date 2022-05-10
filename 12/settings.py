f1b43078940353947e60ead66c52f81b739c142f
ljl <2538686814@qq.com>
2022-05-10 20:40:09 +0800

    3

--- 12/settings.py
+++ 12/settings.py
@@ -4,7 +4,6 @@ class Settings:
     def __init__(self):
         """初始化游戏的设置"""
         # 屏幕设置
-        self.screen_width =1200
-        self.screen_height =800
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (100, 150, 200)
-
--- 12/ship.py
+++ 12/ship.py
@@ -2,18 +2,18 @@ import pygame
 
 
 class Ship:
-    """A class to manage the ship."""
+    """管理飞船的类"""
 
     def __init__(self, ai_game):
-        """Initialize the ship and set its starting position."""
+        """初始化飞船并设置初始位置"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Load the ship image and get its rect.
+        # 加载飞船图像并获取其外接矩阵
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        # Start each new ship at the bottom center of the screen.
+        # 将飞船放在屏幕的底中央
         self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):

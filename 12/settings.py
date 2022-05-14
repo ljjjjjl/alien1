7d13b5b60e0e5a4dacc5472a2be6d927430b494a
ljl <2538686814@qq.com>
2022-05-14 16:54:14 +0800

    12.8.1

--- 12/settings.py
+++ 12/settings.py
@@ -6,7 +6,14 @@ class Settings:
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (100, 150, 200)
+        # self.bg_color = (100, 150, 200)
+        self.bg_color = (200, 200, 200)
 
         # 飞船速度
         self.ship_speed = 1.5
+
+        # 子弹设置
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)

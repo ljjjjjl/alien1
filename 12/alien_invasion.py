5cacf4336e44852059e767050905d461ecea1e5e
ljl <2538686814@qq.com>
2022-05-14 16:46:35 +0800

    12.6.7

--- 12/alien_invasion.py
+++ 12/alien_invasion.py
@@ -33,20 +33,28 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # 向右移动飞船
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    # 向左移动飞船
-                    self.ship.moving_left = True
-                elif event.key == pygame.K_q:
-                    sys.exit()
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 # 松开时才停止移动
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按建"""
+        if event.key == pygame.K_RIGHT:
+            # 向右移动飞船
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # 向左移动飞船
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应松开"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""

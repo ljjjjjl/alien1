dbac219b2eab89231fe8ecd2b522ab7f335ff240
ljl <2538686814@qq.com>
2022-05-15 15:38:41 +0800

    13.6

---
+++ 12~14/game_stats.py
@@ -0,0 +1,14 @@
+class GameStats:
+    """跟踪游戏的统计信息"""
+
+    def __init__(self, ai_game):
+        """初始化统计信息"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        # 活跃状态？
+        self.game_active = True
+
+    def reset_stats(self):
+        """初始化可能变化的统计信息"""
+        self.ships_left = self.settings.ship_limit

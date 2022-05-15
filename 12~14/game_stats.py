c706f483b7b2fb28ae1bf95efd89fb760085129b
ljl <2538686814@qq.com>
2022-05-15 16:18:07 +0800

    14.1

--- 12~14/game_stats.py
+++ 12~14/game_stats.py
@@ -7,7 +7,7 @@ class GameStats:
         self.reset_stats()
 
         # 活跃状态？
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         """初始化可能变化的统计信息"""
--- 12~14/settings.py
+++ 12~14/settings.py
@@ -7,7 +7,7 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         # self.bg_color = (100, 150, 200)
-        self.bg_color = (200, 200, 200)
+        self.bg_color = (230, 230, 230)
 
         # 飞船设置
         self.ship_speed = 1.5

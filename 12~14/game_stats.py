a3f2b4d2cda43451418907a0776de57d31797d3c
ljl <2538686814@qq.com>
2022-05-15 16:45:22 +0800

    14.3

--- 12~14/game_stats.py
+++ 12~14/game_stats.py
@@ -6,9 +6,14 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-        # 活跃状态？
+        # 活跃状态
         self.game_active = False
+
+        # High score should never be reset.
+        self.high_score = 0
 
     def reset_stats(self):
         """初始化可能变化的统计信息"""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
